da4fb68406f0cda07fa9dfa84f7462db0492d8a4
Bernardo Garciarivas <brgarciarivas@gmail.com>
2016-03-31 00:02:15 -0400

    sad boy mechem or however you spell it died

---
+++ src/components/FeelTheEconomy.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import Base from './Base.jsx'
+
+export default class FeelTheEconomy extends Base{
+	render(){
+		return(
+				<div className='feel-the-econ'>
+					<div className='flex-column'>
+						<div className=' landing-top flex-row'>
+							<div className='flex-row landing-top-set'>
+
+								<img id='setmine-logo'src="/img/setmine-logo-white.png"/>
+								<h1 id='setcoins-landing-header'>Setcoins</h1>
+							</div>
+							<div>
+								<h1>BTC</h1>
+							</div>
+						</div>
+						<div className=' landing-bottom flex-row'>
+							<div>
+								jl;
+							</div>
+							<div className='video-container'>
+								out here fam
+							</div>
+						</div>
+					</div>
+				</div>
+			)
+	}
+}
--- src/components/LandingPage.jsx
+++ src/components/LandingPage.jsx
@@ -1,14 +1,15 @@
 import React from 'react';
 import Base from './Base.jsx';
+import FeelTheEconomy from './FeelTheEconomy.jsx';
 
 export default class LandingPage extends Base {
 	render(){
 		return(
-				<div id='landing-page flex-column'>
+				<div id='landing-page'>
 						<div className='landing-page-background'>
-							
+							<FeelTheEconomy />
 						</div>
-						<div>
+						<div className='landing-page-info'>
 							<p>we finna get lit out here fam</p>
 						</div>
 				</div>
